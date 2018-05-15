9199fe4b476ef030d49aad8b6347f13d29c4ffbc
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-15 17:39:14 +0100

    combined multiple reducers

---
+++ src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+  results: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: action.result})
+      }
+    case actionTypes.DELETE_RESULT:
+      // const id = 2;
+      // const newArr = [...state.results];
+      // newArr.splice(id, 1);
+      const newArr = state.results.filter( result => result.id !== action.elemId);
+      return {
+        ...state,
+        results: newArr
+      }
+  }
+
+  return state;
+};
+
+export default reducer;
